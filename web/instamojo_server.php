b4fab6bbf7d9cfa171ccd0ec60aba97ab59211eb
Parth Mudgal <artpar@gmail.com>
2017-08-14 16:33:37 +0530

    that is whatever this is

--- web/instamojo_server.php
+++ web/instamojo_server.php
@@ -1,12 +1,13 @@
 <?php
 
+namespace Sp1\Sp2;
+
 const apiKey = "dNo97yFf7nDcq0QeYElTR5qvpnCssXpFr6BrBU2i";
 const apiSecret = "WttpIlEcGCZVTmQWxkpff0ssQH2BkICAaK0ip0HoB998OKXoUMq4hJfpaccSiTkcUieMNIY826o3MOSKMgqkuspoizMTunLcLiqqgPdihQY885wafIn1JMBLzwtYPEIY";
 const salt = "07c7cd4e440d4e978b9ed5f3392b2011";
 const redirectUrl = "http://google.com";
 const notifyUrl = "http://google.com";
 
-namespace Sp1\Sp2;
 
 class InstamojoClient
 {
