f3340cd19998e47114f80286f6524667155c00e9
Parth Mudgal <artpar@gmail.com>
2017-09-22 06:50:38 +0530

    update handle notification to handle redirect as well

--- web/mid/handle_notification.php
+++ web/mid/handle_notification.php
@@ -2,6 +2,12 @@
 require_once(dirname(__FILE__) . '/Veritrans.php');
 Veritrans_Config::$isProduction = false;
 Veritrans_Config::$serverKey = 'VT-server-jxeozomsTmDnLuRQ2ZQdeNv6';
+
+
+
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+
+
 $notif = new Veritrans_Notification();
 $transaction = $notif->transaction_status;
 $type = $notif->payment_type;
@@ -14,7 +20,7 @@ if ($transaction == 'capture') {
       // TODO set payment status in merchant's database to 'Challenge by FDS'
       // TODO merchant should decide whether this transaction is authorized or not in MAP
       echo "Transaction order_id: " . $order_id ." is challenged by FDS";
-      } 
+      }
       else {
       // TODO set payment status in merchant's database to 'Success'
       echo "Transaction order_id: " . $order_id ." successfully captured using " . $type;
@@ -24,11 +30,11 @@ if ($transaction == 'capture') {
 else if ($transaction == 'settlement'){
   // TODO set payment status in merchant's database to 'Settlement'
   echo "Transaction order_id: " . $order_id ." successfully transfered using " . $type;
-  } 
+  }
   else if($transaction == 'pending'){
   // TODO set payment status in merchant's database to 'Pending'
   echo "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type;
-  } 
+  }
   else if ($transaction == 'deny') {
   // TODO set payment status in merchant's database to 'Denied'
   echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.";
@@ -40,4 +46,42 @@ else if ($transaction == 'settlement'){
   else if ($transaction == 'cancel') {
   // TODO set payment status in merchant's database to 'Denied'
   echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is canceled.";
+}
+
+
+} else {
+
+
+    //
+    // order_id=776981683&status_code=200&transaction_status=capture
+
+    $order_id = $_GET['order_id'];
+    $statusCode = $_GET['status_code'];
+    $transaction  = $_GET['transaction_status'];
+
+
+	if($transaction == 'capture') {
+	  echo "<p>Transaksi berhasil.</p>";
+	  echo "<p>Status transaksi untuk order id : " . $order_id;
+
+	}
+	// Deny
+	else if($transaction == 'deny') {
+	  echo "<p>Transaksi ditolak.</p>";
+	  echo "<p>Status transaksi untuk order id .: " . $order_id;
+
+	}
+	// Challenge
+	else if($transaction == 'challenge') {
+	  echo "<p>Transaksi challenge.</p>";
+	  echo "<p>Status transaksi untuk order id : " . $order_id;
+
+	}
+	// Error
+	else {
+	  echo "<p>Terjadi kesalahan pada data transaksi yang dikirim.</p>";
+	  echo "<p>Status message: [$response->status_code] " . $transaction;
+	}
+
+
 }
